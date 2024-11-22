ae504acfdcd457adf4959c5fb907e95ee8c435a8
mopeneko <mopeneko@lem0n.cc>
2024-11-22 18:09:58 +0900

    Add Skills section

--- src/app/routes/root.tsx
+++ src/app/routes/root.tsx
@@ -1,4 +1,4 @@
-import { Box, Center, Image, SimpleGrid, Text, Title } from "@mantine/core";
+import { Box, Card, Center, Image, List, SimpleGrid, Text, Title } from "@mantine/core";
 import { PageLayout } from "../../components/layouts/page-layout";
 import { width } from "../../config/width";
 
@@ -23,6 +23,48 @@ export const AppRoot = () => {
                         </Center>
                     </Box>
                 </SimpleGrid>
+            </Center>
+
+            <Center pt={32}>
+                <Card w={width}>
+                    <Title order={2}>Skills</Title>
+                    <SimpleGrid pt={32} cols={{ base: 1, sm: 2 }}>
+                        <Card w="100%" h={375} shadow="sm" padding="lg" radius="md" withBorder>
+                            <Title order={3}>Go</Title>
+
+                            <Text pt={16}>実務経験3年。</Text>
+                            <Text>業務ではバックエンド開発を経験し、趣味ではCLIツールを開発しています。</Text>
+                            <Text>Echo, GORMの実務経験があります。</Text>
+                        </Card>
+
+                        <Card w="100%" h={375} shadow="sm" padding="lg" radius="md" withBorder>
+                            <Title order={3}>TypeScript</Title>
+
+                            <Text pt={16}>実務経験3年。</Text>
+                            <Text>業務ではフロントエンド開発やIoTデバイスのデータ処理を経験しました。</Text>
+                            <Text>趣味でもフロントエンド開発に使用しています。</Text>
+                            <Text>Nuxt.js(v2, v3)の実務経験があります。</Text>
+                        </Card>
+
+                        <Card w="100%" h={375} shadow="sm" padding="lg" radius="md" withBorder>
+                            <Title order={3}>Google Cloud</Title>
+
+                            <Text pt={16}>実務経験3年。</Text>
+                            <Text>業務では</Text>
+                            <List>
+                                <List.Item>Compute Engine</List.Item>
+                                <List.Item>Cloud Run</List.Item>
+                                <List.Item>Cloud Functions</List.Item>
+                                <List.Item>Firestore</List.Item>
+                                <List.Item>Firebase Authentication</List.Item>
+                                <List.Item>BigQuery</List.Item>
+                                <List.Item>Cloud Storage</List.Item>
+                                <List.Item>Cloud Build</List.Item>
+                            </List>
+                            <Text>の使用を経験しました。</Text>
+                        </Card>
+                    </SimpleGrid>
+                </Card>
             </Center>
         </PageLayout>
     );
